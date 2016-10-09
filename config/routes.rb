be3d040bface3730099abdc728c324620dfa048e
Lam Tran <tranlamyb95@gmail.com>
2016-10-09 19:34:36 +0700

    remember me

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  root 'static_page#home'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+   root 'static_page#home'
   get 'static_page/home'
 
   get 'static_page/help'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
 
   resources :microposts
   resources :users
